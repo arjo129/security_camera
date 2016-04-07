7fa113041f8eb093a1d52f6f90fe70c5cfcf4d11
Arjo Chakravarty <arjo129@gmail.com>
2016-04-07 12:42:49 +0800

    Simple motion activated security camera

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <opencv/cv.hpp>
+#include <opencv/highgui.h>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+time_t lastmotion;
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+	cv::Mat frame1,frame2,gray1,gray2,diff, thresh, iblur, finalt,bg;
+	bool isBgEnabled=true, confEnabled = false;
+	cv::VideoCapture webcam(0);
+	cv::VideoWriter writer;
+	if(!webcam.isOpened()){
+		cout << "Fatal error: could not open webcam \n";
+		return -1;
+	}
+
+	cv::Mat bim;
+	webcam.read(bim);
+	cv::cvtColor(bim,bg,CV_RGB2GRAY);
+	long i = 0;
+	bool recording = false;
+	while(1){
+		webcam.read(frame1);
+		cv::cvtColor(frame1,gray1, CV_RGB2GRAY);
+		//cout<<sum(frame1)[0]<<"\n";
+		webcam.read(frame2);
+		cv::cvtColor(frame2,gray2, CV_RGB2GRAY);
+		cv::absdiff(gray1,gray2,diff);
+		cv::threshold( diff, thresh, 30, 255,THRESH_BINARY);
+		cv::blur(thresh,iblur, Size(10,10), Point(-1,-1));
+		cv::threshold( iblur, finalt, 2,255,THRESH_BINARY);
+		auto isum = cv::sum(finalt)[0]/(bim.rows*bim.cols);
+		if(isum > 1){
+			if(!recording){
+				cv::Size csize(webcam.get(CV_CAP_PROP_FRAME_WIDTH),webcam.get(CV_CAP_PROP_FRAME_HEIGHT));
+				time_t now = time(NULL);
+				char buffer[80];
+				strftime (buffer,80,"%I%M%S%p_%a_%F.avi",localtime(&now));
+				writer.open(buffer,CV_FOURCC('M','J','P','G'),10,csize);
+				recording = true;
+				cout << "recording to file "<<buffer<<endl;
+			} else {
+				writer << frame1;
+			}
+		}
+		else{
+			cout << "no motion.. recording stopped"<<endl;
+			recording = false;
+		}
+	}
+
+	return 0;
+}
